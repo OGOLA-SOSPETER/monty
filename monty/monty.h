083b714b37295f2b854932101df0526c8db13489
OGOLA-SOSPETER <captainsos483@gmail.com>
2022-09-07 13:35:32 -0700

    monty

--- monty/monty.h
+++ monty/monty.h
@@ -60,5 +60,6 @@ void f_pop(stack_t **head, unsigned int counter);
 void f_swap(stack_t **head, unsigned int counter);
 void f_add(stack_t **head, unsigned int counter);
 void f_nop(stack_t **head, unsigned int counter);
+int execute(char *content, stack_t **stack, unsigned int counter, FILE *file)
 
 #endif
